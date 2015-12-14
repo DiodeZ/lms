38ead8a0401a1e9b6e01c576653e8df75e189fb7
DiodeZ <minhhoang1987@gmail.com>
2015-12-15 01:39:56 +0700

    init repos
    init

---
+++ LedMusicStudio/LedMusicStudio/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LedMusicStudio
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            BassEngine bassEngine = BassEngine.Instance;
+            bassEngine.PropertyChanged += BassEngine_PropertyChanged;
+
+            spectrumAnalyzer.RegisterSoundPlayer(bassEngine);
+            waveformTimeline.RegisterSoundPlayer(bassEngine);
+        }
+
+        private void BassEngine_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            BassEngine engine = BassEngine.Instance;
+            switch (e.PropertyName)
+            {
+                case "FileTag":
+                    if (engine.FileTag != null)
+                    {
+                        TagLib.Tag tag = engine.FileTag.Tag;
+                        if (tag.Pictures.Length > 0)
+                        {
+                            using (MemoryStream albumArtworkMemStream = new MemoryStream(tag.Pictures[0].Data.Data))
+                            {
+                                try
+                                {
+                                    BitmapImage albumImage = new BitmapImage();
+                                    albumImage.BeginInit();
+                                    albumImage.CacheOption = BitmapCacheOption.OnLoad;
+                                    albumImage.StreamSource = albumArtworkMemStream;
+                                    albumImage.EndInit();
+                                    //albumArtPanel.AlbumArtImage = albumImage;
+                                }
+                                catch (NotSupportedException)
+                                {
+                                    //albumArtPanel.AlbumArtImage = null;
+                                    // System.NotSupportedException:
+                                    // No imaging component suitable to complete this operation was found.
+                                }
+                                albumArtworkMemStream.Close();
+                            }
+                        }
+                        else
+                        {
+                            //albumArtPanel.AlbumArtImage = null;
+                        }
+                    }
+                    else
+                    {
+                        //albumArtPanel.AlbumArtImage = null;
+                    }
+                    break;
+                case "ChannelPosition":
+                    clockDisplay.Time = TimeSpan.FromSeconds(engine.ChannelPosition);
+                    break;
+                default:
+                    // Do Nothing
+                    break;
+            }
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (BassEngine.Instance.CanPlay)
+                BassEngine.Instance.Play();
+        }
+
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (BassEngine.Instance.CanPause)
+                BassEngine.Instance.Pause();
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (BassEngine.Instance.CanStop)
+                BassEngine.Instance.Stop();
+
+        }
+
+        private void TestButton_Click(object sender, RoutedEventArgs e)
+        {
+            BassEngine.Instance.OpenFile("E:\\Quen Cach Yeu - Luong Bich Huu.mp3");
+        }
+    }
+}
